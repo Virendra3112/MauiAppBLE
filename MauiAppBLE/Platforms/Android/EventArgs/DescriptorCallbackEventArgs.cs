380f154c429b547596ae4ee18e305b5a1ee5a303
Virendra <virendra.thakur31@gmail.com>
2023-09-19 21:37:19 +0530

    DescriptorCallbackEventArgs added.

---
+++ MauiAppBLE/Platforms/Android/EventArgs/DescriptorCallbackEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using Android.Bluetooth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppBLE.Platforms.Android.EventArgs
+{
+    public class DescriptorCallbackEventArgs
+    {
+        public BluetoothGattDescriptor Descriptor { get; }
+        public Exception Exception { get; }
+
+        public DescriptorCallbackEventArgs(BluetoothGattDescriptor descriptor, Exception exception = null)
+        {
+            Descriptor = descriptor;
+            Exception = exception;
+        }
+    }
+}
