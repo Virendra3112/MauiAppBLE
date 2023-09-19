e8c0889b76d5c53b086e66d0d6fa9775d57d1fc4
Virendra <virendra.thakur31@gmail.com>
2023-09-19 21:39:44 +0530

    MtuRequestCallbackEventArgs added.

---
+++ MauiAppBLE/Platforms/Android/EventArgs/MtuRequestCallbackEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppBLE.Platforms.Android.EventArgs
+{
+    public class MtuRequestCallbackEventArgs : System.EventArgs
+    {
+        public Exception Error { get; }
+        public int Mtu { get; }
+
+        public MtuRequestCallbackEventArgs(Exception error, int mtu)
+        {
+            Error = error;
+            Mtu = mtu;
+        }
+    }
+}
