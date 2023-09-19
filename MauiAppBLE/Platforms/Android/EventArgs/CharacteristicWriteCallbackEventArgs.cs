a24e6e99f69b4df2066cc9c13adb67c29b1964c5
Virendra <virendra.thakur31@gmail.com>
2023-09-19 21:36:41 +0530

    CharacteristicWriteCallbackEventArgs added.

---
+++ MauiAppBLE/Platforms/Android/EventArgs/CharacteristicWriteCallbackEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using Android.Bluetooth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppBLE.Platforms.Android.EventArgs
+{
+    public class CharacteristicWriteCallbackEventArgs
+    {
+        public BluetoothGattCharacteristic Characteristic { get; }
+        public Exception Exception { get; }
+        public GattStatus Status { get; }
+
+        public CharacteristicWriteCallbackEventArgs(BluetoothGattCharacteristic characteristic, GattStatus status, Exception exception = null)
+        {
+            Characteristic = characteristic;
+            Status = status;
+            Exception = exception;
+        }
+    }
+}
