dc01481f4592d84449981f2f26cfc076d1e530c1
Virendra <virendra.thakur31@gmail.com>
2023-09-19 21:39:57 +0530

    commit

---
+++ MauiAppBLE/Platforms/Android/EventArgs/ServicesDiscoveredCallbackEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppBLE.Platforms.Android.EventArgs
+{
+    public class ServicesDiscoveredCallbackEventArgs
+    {   public ServicesDiscoveredCallbackEventArgs()
+        {
+
+        }
+    }
+}
