ea17160ea7b016a1085c612b3cb2dfceec8b2540
Virendra <virendra.thakur31@gmail.com>
2023-09-19 21:35:50 +0530

    CharacteristicReadCallbackEventArgs and IGattCallback added for android.

---
+++ MauiAppBLE/Platforms/Android/EventArgs/CharacteristicReadCallbackEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using Android.Bluetooth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppBLE.Platforms.Android.EventArgs
+{
+    public class CharacteristicReadCallbackEventArgs
+    {
+        public BluetoothGattCharacteristic Characteristic { get; }
+        public GattStatus Status { get; }
+
+        public CharacteristicReadCallbackEventArgs(BluetoothGattCharacteristic characteristic, GattStatus status)
+        {
+            Characteristic = characteristic;
+            Status = status;
+        }
+    }
+}
