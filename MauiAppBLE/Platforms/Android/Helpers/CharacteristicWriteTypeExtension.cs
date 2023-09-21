9ebda54f18c574917b801043eba9240e859834fc
Virendra <virendra.thakur31@gmail.com>
2023-09-21 22:04:59 +0530

    commit

---
+++ MauiAppBLE/Platforms/Android/Helpers/CharacteristicWriteTypeExtension.cs
@@ -0,0 +1,26 @@
+﻿using Android.Bluetooth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppBLE.Platforms.Android.Helpers
+{
+    public static class CharacteristicWriteTypeExtension
+    {
+        //public static GattWriteType ToNative(this CharacteristicWriteType writeType)
+        //{
+        //    //switch (writeType)
+        //    //{
+        //    //    case CharacteristicWriteType.WithResponse:
+        //    //        return GattWriteType.Default;
+        //    //    case CharacteristicWriteType.WithoutResponse:
+        //    //        return GattWriteType.NoResponse;
+        //    //    default:
+        //    //        throw new NotImplementedException();
+        //    //}
+
+        //}
+    }
+}
