41627935ed9928cf773a157cd38a800f28359b63
Virendra <virendra.thakur31@gmail.com>
2023-09-22 21:12:23 +0530

    commit

---
+++ MauiAppBLE/Platforms/Android/Implementations/GattCallback.cs
@@ -0,0 +1,16 @@
+﻿using MauiAppBLE.Platforms.Android.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppBLE.Platforms.Android.Implementations
+{
+    internal class GattCallback : IGattCallback
+    {
+        public GattCallback()
+        {
+        }
+    }
+}
