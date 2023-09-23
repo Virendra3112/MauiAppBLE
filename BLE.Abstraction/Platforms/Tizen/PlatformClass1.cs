15c238ddf5650fe7ccc5686b09c1d3d7acb3117f
Virendra <virendra.thakur31@gmail.com>
2023-09-23 23:20:56 +0530

    commit

---
+++ BLE.Abstraction/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BLE.Abstraction
+{
+    // All the code in this file is only included on Tizen.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ BLE.Abstraction/Platforms/Windows/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace BLE.Abstraction
+{
+    // All the code in this file is only included on Windows.
+    public class PlatformClass1
+    {
+    }
+}
