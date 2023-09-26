ec347499180f94ee02a9153b50e3fda529dade3f
Virendra <virendra.thakur31@gmail.com>
2023-09-26 21:10:32 +0530

    IScanner updated.

--- BLE.Shared/Interfaces/IScanner.cs
+++ BLE.Shared/Interfaces/IScanner.cs
@@ -11,8 +11,14 @@ namespace BLE.Shared.Interfaces
         bool IsScanning { get; }
 
         void StartScan();
+        void StartScan(Guid[] guids = null);
 
         void StopScan();
         void TimeoutScan();
+
+        void ReDiscoverDevices(IDevice device);
+
+        event EventHandler DevicesDiscovered;
+        event EventHandler ScanTimeout;
     }
 }
