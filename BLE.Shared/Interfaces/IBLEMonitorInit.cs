4cadd4b282b59f996b4845ecf6fc1f67e673d847
Virendra <virendra.thakur31@gmail.com>
2023-09-26 21:12:03 +0530

    IBLEMonitorInit added.

---
+++ BLE.Shared/Interfaces/IBLEMonitorInit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLE.Shared.Interfaces
+{
+    public interface IBLEMonitorInit : IDisposable
+    {
+        void onInit();
+
+        void Clean();
+
+        bool IsInit { get; }
+    }
+}
