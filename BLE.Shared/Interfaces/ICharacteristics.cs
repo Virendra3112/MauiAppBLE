a8c6d7d74e4e1694e4159f11ca958bbf186df0d2
Virendra <virendra.thakur31@gmail.com>
2023-09-27 21:56:47 +0530

    ICharacteristics updated

--- BLE.Shared/Interfaces/ICharacteristics.cs
+++ BLE.Shared/Interfaces/ICharacteristics.cs
@@ -8,5 +8,26 @@ namespace BLE.Shared.Interfaces
 {
     public interface ICharacteristics : IBleObject
     {
+        bool CanRead { get; }
+        bool CanWrite { get; }
+        bool CanNotify { get; }
+
+        bool CanNotifyAll { get; }
+
+        IBLEService bLEService { get; }
+
+        byte[] Value { get; }
+
+        Task<bool> Read();
+
+        Task<bool> Notify();
+        Task<bool> Write(byte[] value);
+
+        void Write2(byte[] value);
+
+        event EventHandler Notified;
+
+
+
     }
 }
