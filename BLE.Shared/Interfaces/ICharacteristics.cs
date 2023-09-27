597f64b287470d5280e0c2faf1b5a49f7c9ae0ed
Virendra <virendra.thakur31@gmail.com>
2023-09-27 21:57:47 +0530

    commit.

--- BLE.Shared/Interfaces/ICharacteristics.cs
+++ BLE.Shared/Interfaces/ICharacteristics.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BLE.Shared.Interfaces
+﻿namespace BLE.Shared.Interfaces
 {
     public interface ICharacteristics : IBleObject
     {
