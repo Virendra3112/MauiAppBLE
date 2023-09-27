0559b5ace7063ca35d3f6c30d4bb5fab83a0c95b
Virendra <virendra.thakur31@gmail.com>
2023-09-27 22:02:34 +0530

    IAdapter updated.

--- BLE.Shared/Interfaces/IAdapter.cs
+++ BLE.Shared/Interfaces/IAdapter.cs
@@ -2,5 +2,16 @@
 {
     public interface IAdapter : IBleObject
     {
+        string AdapterState { get; }
+
+        event EventHandler AdapterStateChanged;
+
+        Task<bool> ConnectBLEDevice(IDevice device);
+
+        Task DisConnectBLEDevice(IDevice device);
+
+        Task CancelPendingConnections();
+
+        IDevice GetDeviceByUID(string uuid);
     }
 }
